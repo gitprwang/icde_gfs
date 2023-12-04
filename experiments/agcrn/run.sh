79d084f6e7f6b32f48246703bdfdc8264552f065
gitprwang <w964195365@live.com>
2023-12-03 20:20:16 -0600

    Add files via upload

---
+++ experiments/agcrn/run.sh
@@ -0,0 +1,5 @@
+python experiments/agcrn/main.py --device cuda:1 --dataset CA --years 2019 --model_name agcrn --seed 2018 --bs 32
+
+# python experiments/agcrn/main.py --device cuda:2 --dataset GBA --years 2019 --model_name agcrn --seed 2018 --bs 64
+
+# python experiments/agcrn/main.py --device cuda:2 --dataset SD --years 2019 --model_name agcrn --seed 2018 --bs 64
---
+++ experiments/ddgcrn/run.sh
@@ -0,0 +1,1 @@
+python experiments/ddgcrn/main.py --device cuda:0 --dataset CA --years 2019 --model_name ddgcrn --seed 2018 --bs 2
